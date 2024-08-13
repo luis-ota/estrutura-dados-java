a2726b623760052ee82946e2b53ff454f47ebe44
Luis Otavio Silva Santos <otavio.l@pucpr.edu.br>
2024-08-13 19:17:38 -0300

    init

---
+++ Stack.java
@@ -0,0 +1,12 @@
+public interface Stack<T> {
+    public void push();
+    public T pop();
+    public void clear();
+    public int len();
+    public boolean isEmpty();
+    public boolean isFull();
+
+
+
+
+}
